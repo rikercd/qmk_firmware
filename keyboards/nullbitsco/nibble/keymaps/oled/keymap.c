b5e71c10fb43da7e76f0e235b8ea8dfee28ad78d
Christopher Riker <chrisriker@outlook.com>
2021-04-10 09:55:41 -0400

    Implement VIA firmware with luna support. All OLED support currently not operational

--- keyboards/nullbitsco/nibble/keymaps/oled/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/oled/keymap.c
@@ -40,7 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 #ifdef OLED_DRIVER_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    oled_on();
+    return OLED_ROTATION_90;
+}
 
 static void render_logo(void) {
     static const char PROGMEM nibble_logo[] = {
